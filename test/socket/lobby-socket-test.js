8e4d5f873cb1b12a571675ab82cc96d80ef1e7b4
Christofer Jungberg <kiffen_@hotmail.com>
2018-01-15 14:46:52 +0100

    added test for database

--- test/socket/lobby-socket-test.js
+++ test/socket/lobby-socket-test.js
@@ -13,7 +13,7 @@
 //
 //
 //
-// var socketURL = 'http://localhost:1313';
+// var socketURL = 'http://localhost:3000';
 // var options = {
 //     transports: ['websocket'],
 //     'force new connection': true
@@ -22,7 +22,7 @@
 //
 // describe("Test Lobby", function(done) {
 //     before(function() {
-//         server.listen(1313);
+//         server.listen(3000);
 //         chat(io);
 //     });
 //
@@ -31,25 +31,25 @@
 //         done();
 //     });
 //
-//     // it('should broadcast to all', function(done){
-//     //     var client = ioClient.connect(socketURL, options);
-//     //     var client2 = ioClient.connect(socketURL, options);
-//     //
-//     //
-//     //     client.on("connect", function() {
-//     //         client2.on("connect", function() {
-//     //             client.emit("chat message", {from: "svensson", message: "hej"});
-//     //             client.disconnect();
-//     //         });
-//     //     });
-//     //
-//     //     client2.on("chat message", function(data) {
-//     //         assert.equal(data.from, "svensson");
-//     //         assert.equal(data.message, "hej");
-//     //         client2.disconnect();
-//     //         done();
-//     //     })
-//     // });
+//     it('should broadcast to all', function(done){
+//         var client = ioClient.connect(socketURL, options);
+//         var client2 = ioClient.connect(socketURL, options);
+//
+//
+//         client.on("connect", function() {
+//             client2.on("connect", function() {
+//                 client.emit("chat message", {from: "svensson", message: "hej"});
+//                 client.disconnect();
+//             });
+//         });
+//
+//         client2.on("chat message", function(data) {
+//             assert.equal(data.from, "svensson");
+//             assert.equal(data.message, "hej");
+//             client2.disconnect();
+//             done();
+//         })
+//     });
 //
 //     it('should add user serverInfo', function(done){
 //         var client = ioClient.connect(socketURL, options);
@@ -68,39 +68,39 @@
 //
 //                 client.emit("new room", room);
 //                 client.disconnect();
+//
+//                 client2.on("user list", function(data) {
+//                     // assert.equal(data.games[0].name, "test room");
+//                     assert.equal(data.games[0].skill, "low");
+//                     assert.equal(data.games[0].id, 100);
+//                     client2.disconnect();
+//                     done();
+//                 })
 //             });
 //         });
-//
-//         client2.on("user list", function(data) {
-//             assert.equal(data.games[0].name, "test room");
-//             assert.equal(data.games[0].skill, "low");
-//             assert.equal(data.games[0].id, 100);
-//             client2.disconnect();
-//             done();
-//         })
 //     });
 //
-//
-//     it('should add room to serverIfno', function(done){
-//         var client = ioClient.connect(socketURL, options);
-//         var client2 = ioClient.connect(socketURL, options);
-//
-//
-//         client.on("connect", function() {
-//             client2.on("connect", function() {
-//                 client.emit("joined room", 100);
-//                 client.disconnect();
-//                 client2.disconnect();
-//                 done();
-//             });
-//         });
-//
-//         // client2.on("user list", function(data) {
-//         //     assert.equal(data.games[0].connected, 1);
-//         //     client2.disconnect();
-//         //     done();
-//         // })
-//     });
+//     //
+//     // it('should add room to serverIfno', function(done){
+//     //     var client = ioClient.connect(socketURL, options);
+//     //     var client2 = ioClient.connect(socketURL, options);
+//     //
+//     //
+//     //     client.on("connect", function() {
+//     //         client2.on("connect", function() {
+//     //             client.emit("joined room", 100);
+//     //             client.disconnect();
+//     //             client2.disconnect();
+//     //             done();
+//     //         });
+//     //     });
+//     //
+//     //     // client2.on("user list", function(data) {
+//     //     //     assert.equal(data.games[0].connected, 1);
+//     //     //     client2.disconnect();
+//     //     //     done();
+//     //     // })
+//     // });
 //
 //
 // });
